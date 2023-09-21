b40cfffaa18c9da4085c5b13404fd06f2212c09a
Emon <emon.mhk69@gmail.com>
2023-09-22 01:36:55 +0600

    basic backend ready

--- index.js
+++ index.js
@@ -33,6 +33,28 @@ async function run() {
 
         // all queries
 
+        // get all users data:
+        app.get("/users", async (req, res) => {
+
+            const cars = await usersCollections.find().toArray();
+
+            if (req.query?.sort === 'desc') {
+                cars.sort((a, b) => b.price - a.price); // Sort in descending order based on price
+            } else {
+                cars.sort((a, b) => a.price - b.price); // Sort in ascending order based on price
+            }
+            res.send(cars);
+        })
+
+
+        // get single user:
+        app.get("/users/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await usersCollections.findOne(query);
+            res.send(result);
+        })
+
 
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
